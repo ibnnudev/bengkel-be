e215d64548f0e622fc42acdf007f6e076b5e5821
Moh Ibnu Abdurrohman Sutio <moh.ibnu@admo.astra.co.id>
2026-03-23 09:22:17 +0700

    feat: add user, vehicle, and SOS request models with corresponding enums and migrations

--- index.ts
+++ index.ts
@@ -1,1 +1,12 @@
-console.log("Hello via Bun!");
+import express from "express";
+import cors from "cors";
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.json());
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
